440e06baacfe2bc994be5b2dabbb392933a553fa
Gigi M <mmgamao1@gmail.com>
2019-08-10 21:34:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,5 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/' do
     "Hello, World!"
+  end  
+
+  get '/' do
+    "My name is__"
   end
+  
+  get '/' do
+    "My hometown is__"
+  end
+
+end
+
 end
